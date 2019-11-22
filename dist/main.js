fc8747a48ad6ff7585c3dd3918ffa18126c3efba
Dinesh <dineshnarendrapatil@gmail.com>
2019-11-22 14:47:52 +0530

    added servr url

--- dist/main.js
+++ dist/main.js
@@ -905,7 +905,7 @@ if (window.location.hostname.indexOf("localhost") > -1) {
 }
 else { //dev server
     if (window.location.hostname.indexOf("sid") > -1) {
-        serverUrlEnv = ""; //"https://sidapp.com"
+        serverUrlEnv = "http://3.135.146.133:3000"; //"https://sidapp.com"
     }
 }
 var serverUrl = serverUrlEnv;
